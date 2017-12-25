6c8735b7a6659342faf15d6886acfaa79e9de56d
Douglas Phillips <flopcode@protonmail.ch>
2017-12-25 21:47:42 +0100

    Initial Commit

---
+++ test/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule Personal.PageControllerTest do
+  use Personal.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
