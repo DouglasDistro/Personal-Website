7fb84b090a3496adbe3969a20ecdd3508ac02a14
Douglas Phillips <flopcode@protonmail.ch>
2017-12-25 21:48:55 +0100

    Initial Commit

---
+++ lib/personal.ex
@@ -0,0 +1,31 @@
+defmodule Personal do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Start the Ecto repository
+      supervisor(Personal.Repo, []),
+      # Start the endpoint when the application starts
+      supervisor(Personal.Endpoint, []),
+      # Start your own worker by calling: Personal.Worker.start_link(arg1, arg2, arg3)
+      # worker(Personal.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Personal.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    Personal.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
