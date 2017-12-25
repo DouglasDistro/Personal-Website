ccb3566ba11c6dc66e4d1f3128a5adb5221bc938
Douglas Phillips <flopcode@protonmail.ch>
2017-12-25 21:46:15 +0100

    Initial Commit

---
+++ web/controllers/page_controller.ex
@@ -0,0 +1,7 @@
+defmodule Personal.PageController do
+  use Personal.Web, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+end
---
+++ web/router.ex
@@ -0,0 +1,26 @@
+defmodule Personal.Router do
+  use Personal.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Personal do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Personal do
+  #   pipe_through :api
+  # end
+end
